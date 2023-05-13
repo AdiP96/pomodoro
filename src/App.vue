a892a02d5052327a61328c69db3ce9ee02f9b920
Petran Adrian <adrian@Petrans-MacBook-Pro.local>
2023-05-13 18:20:04 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import Pomodoro from './components/Pomodoro.vue'
+</script>
+
+<template>
+  <main>
+    <Pomodoro />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+main {
+  background-color: red;
+  height: 100vh;
+  width: 100vw;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Pomodoro.vue
@@ -0,0 +1,194 @@
+<template>
+    <v-container fluid full-height class="main-container">
+        <div class="navbar">
+            <div style="font-weight: bold; color: white">ADPomodoro</div>
+            <div>
+                <button class="nav-button" @click="onAuthorClick">Author</button>
+            </div>
+        </div>
+        <br />
+        <br />
+        <div class="timer-container">
+            <div class="timer-box">
+                <div>
+                    <template v-for="(type, index) in Object.keys(types)" :key="index">
+                        <button @click="() => changeType(type)"
+                            :class="selectedType === type ? 'selected-type-button' : 'timer-type-button'">{{
+                                type }}</button>
+                    </template>
+                </div>
+                <br>
+                <div :class="`timer ${selectedType === 'Pomodoro' ? '' : ' timer-break '}`">
+                    {{ minutes }} : {{ seconds < 10 ? '0' : '' }}{{ seconds }} </div>
+                        <br>
+                        <button class="start-button" @click="onStart">{{ !interval ? 'Start' : 'Reset' }}</button>
+                </div>
+            </div>
+    </v-container>
+</template>
+
+<script lang="ts">
+import start_work from '../assets/start_work.mp3';
+import start_pause from '../assets/start_pause.mp3';
+export default {
+    data() {
+        return {
+            selectedType: 'Pomodoro',
+            types: { Pomodoro: 25, "Short break": 5, "Long break": 15 },
+            minutes: 0,
+            seconds: 0,
+            interval: null,
+        }
+    },
+
+    beforeMount() {
+        this.onReset();
+    },
+
+    computed: {
+    },
+
+    methods: {
+        changeType(type: string) {
+            this.selectedType = type;
+            this.onReset();
+        },
+
+        onSettingsClick() {
+            console.log('clicked')
+        },
+
+        onReset() {
+            if (this.interval)
+                window.clearInterval(this.interval);
+            this.interval = null;
+            this.seconds = 0;
+            this.minutes = this.types[this.selectedType]
+        },
+
+        onStart() {
+            if (this.interval) {
+                this.onReset();
+                return
+            }
+            if (this.selectedType === 'Pomodoro') {
+                this.playSound(start_work);
+            } else {
+                this.playSound(start_pause);
+            }
+            this.interval = window.setInterval(() => {
+                this.countDown();
+            }, 1000)
+        },
+
+        countDown() {
+            if (this.minutes === 0 && this.seconds === 0) {
+                if (this.selectedType === 'Pomodoro') {
+                    this.selectedType = "Long break";
+                } else {
+                    this.selectedType = "Pomodoro";
+                }
+                this.onReset();
+                this.onStart();
+            }
+            if (this.seconds === 0) {
+                this.minutes--;
+                this.seconds = 59;
+            } else {
+                this.seconds--;
+            }
+        },
+
+        playSound(sound: any) {
+            const audio = new Audio(sound);
+            audio.play();
+        },
+
+        onAuthorClick() {
+            alert('Author: Adrian Petran');
+        }
+
+
+    }
+}
+</script>
+
+<style>
+.main-container {
+    display: flex;
+    flex-direction: column;
+}
+
+.navbar {
+    display: flex;
+    justify-content: space-around;
+    padding: 24px;
+}
+
+button {
+    padding: 12px;
+    font-weight: 500;
+    margin-left: 4px;
+    margin-right: 4px;
+    padding-top: 6px;
+    padding-bottom: 6px;
+    border-radius: 4px;
+    border: none;
+    cursor: pointer;
+}
+
+.nav-button {
+    background-color: rgba(255, 255, 255, .5);
+    color: white;
+}
+
+.timer-type-button {
+    background-color: rgba(255, 255, 255, 0);
+    color: white;
+    font-size: 18px;
+}
+
+.selected-type-button {
+    background-color: rgba(255, 255, 255, 0.3);
+    color: white;
+    font-size: 18px;
+    font-weight: bold;
+}
+
+.timer-container {
+    display: flex;
+    justify-content: center;
+}
+
+.timer-box {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: rgba(255, 255, 255, 0.5);
+    width: 500px;
+    padding: 24px;
+    border-radius: 10px;
+}
+
+.timer {
+    color: white;
+    font-weight: bold;
+    font-size: 112px;
+}
+
+.timer-break {
+    color: lightgreen !important;
+}
+
+.start-button {
+    color: red;
+    background-color: white;
+    font-weight: bold;
+    font-size: 32px;
+    padding: 12px;
+    padding-left: 64px;
+    padding-right: 64px;
+    min-width: 100px;
+    border-radius: 10px;
+}
+</style>
